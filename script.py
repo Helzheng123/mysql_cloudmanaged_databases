e18d80451c7e9a12407bf6fe741082cfa9240554
helzheng123 <helen.zheng@stonybrook.edu>
2023-10-05 19:19:18 +0000

    update on the connection with database

--- script.py
+++ script.py
@@ -3,25 +3,49 @@ from dotenv import load_dotenv
 from pandas import read_sql
 from sqlalchemy import create_engine, inspect
 
-load_dotenv() 
+"""
+
+This script uses the pymysql library for connecting to MySQL, 
+so you might need to install that (pip install pymysql) if you haven't already.
+
+It also uses python-dotenv for bringing in secrets from your .env file 
+
+The .env should have the following in it:
+
+DB_HOST=your_host
+DB_DATABASE=your_database_name
+DB_USERNAME=your_username
+DB_PASSWORD=your_password
+DB_PORT=3306
+DB_CHARSET=utf8mb4
+
+The default port is set to 3306 for MySQL, but you can override it by 
+modifying the DB_PORT in your .env file.
+
+The connection string is MySQL-specific, incorporating the specified port and charset.
+
+"""
+
+load_dotenv()  # Load environment variables from .env file
 
 # Database connection settings from environment variables
 DB_HOST = os.getenv("DB_HOST")
 DB_DATABASE = os.getenv("DB_DATABASE")
 DB_USERNAME = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_PORT = int(os.getenv("DB_PORT", 3306))
-DB_CHARSET = os.getenv("DB_CHARSET", "utf8mb4")
+#DB_PORT = int(os.getenv("DB_PORT", 3306))
+#DB_CHARSET = os.getenv("DB_CHARSET", "utf8mb4")
 
 # Connection string
-conn_string = (
-    f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
-    f"?charset={DB_CHARSET}"
+connect_args={'ssl':{'fake_flag_to_enable_tls': True}}
+
+connection_string = f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}'
+
+engine = create_engine(
+        connection_string,
+        connect_args=connect_args,
+
 )
-
-# Create a database engine
-db_engine = create_engine(conn_string, echo=False)
-
 
 def get_tables(engine):
     """Get list of tables."""
@@ -32,10 +56,11 @@ def execute_query_to_dataframe(query: str, engine):
     """Execute SQL query and return result as a DataFrame."""
     return read_sql(query, engine)
 
+
 # Example usage
-tables = get_tables(db_engine)
+tables = get_tables(engine)
 print("Tables in the database:", tables)
 
-sql_query = "SELECT * FROM demographics"  # Modify as per your table
-df = execute_query_to_dataframe(sql_query, db_engine)
+sql_query = "SELECT * FROM appointments"  # Modify as per your table
+df = execute_query_to_dataframe(sql_query, engine)
 print(df)
